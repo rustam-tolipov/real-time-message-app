4c975ab4318b176cb4030479728f10d74a623b0e
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-04-24 02:17:40 +0500

    Add user to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
