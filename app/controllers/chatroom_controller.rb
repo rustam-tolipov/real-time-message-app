5f10e13fef1cde3453f1c8c5d5f3995df9249976
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-04-23 18:15:57 +0500

    Update login

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,5 +1,9 @@
 class ChatroomController < ApplicationController
+    before_action :require_user, except: [:show, :index]
+
+
   def index
+    @message = Message.new
     @messages = Message.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
 end
