89f0cf387da8106eb592cab613cc0b6d71d6618f
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-04-23 18:16:07 +0500

    Create message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user, :logged_in?
+    helper_method :current_user, :logged_in?
 
-  def current_user
+    def current_user
     if session[:user_id]
-      @current_user ||= User.find_by_id(session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
-  def logged_in?
+    def logged_in?
     !current_user.nil?
   end
 
   def require_user
     if !logged_in?
-      flash[:danger] = "You must be logged in"
+      flash[:danger] = "You must be logged in to perform that action"
       redirect_to login_path
     end
   end
