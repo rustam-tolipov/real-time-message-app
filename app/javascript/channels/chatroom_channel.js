238b856b9c2b3b212fe4ae5ba49f26c316fa3da7
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-04-24 05:58:11 +0500

    Update chatroom_channel.js

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -13,8 +13,12 @@ consumer.subscriptions.create('ChatroomChannel', {
     // #message-container is the id of the div that will contain the messages
     // {foo: 'fsadfdsafsadf'}
     console.log(data.mod_message);
-
+    const chatBox = document.querySelector('.chat-box');
     const messageDiv = document.getElementById('message-container');
+    const messageInput = document.getElementById('message-input');
     messageDiv.innerHTML += `<p>${data.mod_message}</p>`;
+    messageInput.value = '';
+
+    chatBox.scrollTop = chatBox.scrollHeight;
   },
 });
