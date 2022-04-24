4981883bc3daaaa9c2e3226af341c69ed1e1227d
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-04-24 13:26:34 +0500

    Add JS functionality to separate mine and their message

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -12,11 +12,24 @@ consumer.subscriptions.create('ChatroomChannel', {
   received(data) {
     // #message-container is the id of the div that will contain the messages
     // {foo: 'fsadfdsafsadf'}
-    console.log(data.mod_message);
+    console.log(data);
+
+    const element = document.getElementById('user-id');
+    const user_id = Number(element.getAttribute('data-user-id'));
+    console.log(user_id);
+
+    let html;
+
+    if (user_id === data.message.user_id) {
+      html = data.mine;
+    } else {
+      html = data.theirs;
+    }
+
     const chatBox = document.querySelector('.chat-box');
     const messageDiv = document.getElementById('message-container');
     const messageInput = document.getElementById('message-input');
-    messageDiv.innerHTML += `<p>${data.mod_message}</p>`;
+    messageDiv.innerHTML += html;
     messageInput.value = '';
 
     chatBox.scrollTop = chatBox.scrollHeight;
